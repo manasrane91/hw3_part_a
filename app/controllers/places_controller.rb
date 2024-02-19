eca930fc47e1322a5ee145f2c110f9ed8d430515
Manas Rane <manas.rane@kellogg.northwestern.edu>
2024-02-19 01:51:53 +0000

    750pm

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,11 +5,24 @@ class PlacesController < ApplicationController
   end
 
   def show
+    @place = Place.find_by({ "id" => params["id"] })
   end
 
   def new
   end
 
-  
+  def create
+    @place.Place.new
+
+    @place["name"] = params["name"]
+    @place["title"] = params["title"]
+    @place["description"] = params["description"]
+    @place["date"] = params["date"]
+
+
+    @place.save
+
+    redirect_to "/places"
+
 
 
